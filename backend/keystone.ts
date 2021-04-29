d214496e5eeadeca0f0c466ccd46fb948273c119
Andres <andresrosalespinzon@gmail.com>
2021-04-29 17:16:38 +0200

    back end created and use of keystone to reach atlas db

---
+++ backend/keystone.ts
@@ -0,0 +1,32 @@
+import 'dotenv/config';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360, // sign in time
+  secret: process.env.COOKIE_SECRET,
+};
+
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL,
+    // TODO: dd data seeding here
+  },
+  lists: createSchema({
+    // schema items go here
+  }),
+  ui: {
+    // TODO: change this for roles
+    isAccessAllowed: () => true,
+  },
+  // TODO: add session value here
+});
--- frontend/pages/orders.js
+++ frontend/pages/orders.js
@@ -1,7 +1,7 @@
 export default function OrderPage() {
   return (
     <div>
-      <p>Hello</p>
+      <p>Hello from orders</p>
     </div>
   );
 }

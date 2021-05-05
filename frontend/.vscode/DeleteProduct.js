2f29113c34668bbb04f84c1ae290309e4eb72238
Andres Rosales <andresrosalespinzon@gmail.com>
2021-05-05 16:59:42 +0200

    pagination dynamic routing

--- frontend/.vscode/DeleteProduct.js
+++ frontend/.vscode/DeleteProduct.js
@@ -2,26 +2,27 @@ import { useMutation } from '@apollo/client';
 import gql from 'graphql-tag';
 
 const DELETE_PRODUCT_MUTATION = gql`
-mutation DELETE_PRODUCT_MUTATION($id:ID!){
-deleteProduct(id:$id){
-    id
-    name
+  mutation DELETE_PRODUCT_MUTATION($id: ID!) {
+    deleteProduct(id: $id) {
+      id
+      name
     }
-}
+  }
 `;
 
 function update(cache, payload) {
-    cache.evict(cache.identify(payload.data.deleteProduct))
+  cache.evict(cache.identify(payload.data.deleteProduct));
 }
 
 export default function DeleteProduct({ id, children }) {
   const [deleteProduct, { loading }] = useMutation(DELETE_PRODUCT_MUTATION, {
-    variables: { id },update:update
+    variables: { id },
+    update: update,
   });
   return (
     <button
-          type="button"
-          disabled={loading}
+      type="button"
+      disabled={loading}
       onClick={() => {
         if (confirm('Are you sure you want to delete this product?')) {
           // delete it

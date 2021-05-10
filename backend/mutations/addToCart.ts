a2b080ed45b1c40a2938ad4e80a4f69369048c70
Andres Rosales <andresrosalespinzon@gmail.com>
2021-05-10 17:41:34 +0200

    Cart - Animating the Cart Cart Value

--- backend/mutations/addToCart.ts
+++ backend/mutations/addToCart.ts
@@ -1,39 +1,49 @@
 /* eslint-disable */
-import { KeystoneContext } from '@keystone-next/types';
+import { KeystoneContext, SessionStore } from '@keystone-next/types';
+import { CartItem } from '../schemas/CartItem';
 import { Session } from '../types';
 
-export default async function addToCart(
+import { CartItemCreateInput } from '../.keystone/schema-types';
+
+async function addToCart(
   root: any,
   { productId }: { productId: string },
   context: KeystoneContext
-) {
-  // Query the current user, see if they are signed in
+): Promise<CartItemCreateInput> {
+  console.log('ADDING TO CART!');
+  // 1. Query the current user see if they are signed in
   const sesh = context.session as Session;
   if (!sesh.itemId) {
-    throw new Error('You must be logged in to do this');
+    throw new Error('You must be logged in to do this!');
   }
-  // query current user cart
-  const allCartItens = await context.lists.CartItem.findMany({
+  // 2. Query the current users cart
+  const allCartItems = await context.lists.CartItem.findMany({
     where: { user: { id: sesh.itemId }, product: { id: productId } },
-    resolveField: 'id, quantity',
+    resolveFields: 'id,quantity',
   });
-  const [existingCartItem] = allCartItens;
+
+  const [existingCartItem] = allCartItems;
   if (existingCartItem) {
+    console.log(existingCartItem);
     console.log(
       `There are already ${existingCartItem.quantity}, increment by 1!`
     );
-    // see if current item is in the cart: if it is increment, if NOT create it
+    // 3. See if the current item is in their cart
+    // 4. if itis, increment by 1
     return await context.lists.CartItem.updateOne({
       id: existingCartItem.id,
       data: { quantity: existingCartItem.quantity + 1 },
+      resolveFields: false,
     });
   }
+  // 4. if it isnt, create a new cart item!
   return await context.lists.CartItem.createOne({
     data: {
-      product: {
-        connect: { id: productId },
-      },
+      product: { connect: { id: productId } },
       user: { connect: { id: sesh.itemId } },
     },
+    resolveFields: false,
   });
 }
+
+export default addToCart;
